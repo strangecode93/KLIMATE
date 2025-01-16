36a382dc44c29c9c23f381db22a577c085a8b958
strange <strangecode93@gmail.com>
2025-01-17 00:28:34 +0530

    some fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Derive __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // Adjust the path as per your project structure
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
